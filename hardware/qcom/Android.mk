7de8599c76483db793b77b0b0afc4fccdd0d3b71
sjllls <2012ji123@gmail.com>
2020-03-21 00:05:16 +0800

    hardware: Use lineage json-c

--- hardware/qcom/Android.mk
+++ hardware/qcom/Android.mk
@@ -11,7 +11,7 @@ MSM_VIDC_TARGET_LIST := msm8996 msm8998 sdm660 sdm845
 #List of targets that use master side content protection
 MASTER_SIDE_CP_TARGET_LIST := msm8996 msm8998 sdm660 sdm845
 
-audio-hal := hardware/qcom/audio
+audio-hal := hardware/qcom/audio/default
 gps-hal := hardware/qcom/gps/sdm845
 display-hal := hardware/qcom/display/msm8998
 QCOM_MEDIA_ROOT := hardware/qcom/media/msm8998
