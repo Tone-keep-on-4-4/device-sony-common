c8b98a4bf46ef13101403cafc05a15699cacf060
sjllls <2012ji123@gmail.com>
2020-03-20 19:54:57 +0800

    common-prop.mk: Set privapp permissions to log

--- common-prop.mk
+++ common-prop.mk
@@ -160,7 +160,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Priv-app permisisons
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.control_privapp_permissions=enforce
+    ro.control_privapp_permissions=log
 
 # Perform color transform on the client
 PRODUCT_PROPERTY_OVERRIDES += \
