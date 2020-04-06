e8ab8454b9407fc631d7ec1874f2ad1d96446013
sjllls <2012ji123@gmail.com>
2020-04-06 10:17:59 +0800

    hardware: Switch to pravite gnss hal

--- hardware/qcom/Android.mk
+++ hardware/qcom/Android.mk
@@ -12,7 +12,6 @@ MSM_VIDC_TARGET_LIST := msm8996 msm8998 sdm660 sdm845
 MASTER_SIDE_CP_TARGET_LIST := msm8996 msm8998 sdm660 sdm845
 
 audio-hal := hardware/qcom/audio/default
-gps-hal := hardware/qcom/gps/sdm845
 display-hal := hardware/qcom/display/msm8998
 QCOM_MEDIA_ROOT := hardware/qcom/media/msm8998
 OMX_VIDEO_PATH := mm-video-v4l2
@@ -27,6 +26,5 @@ include device/sony/common/hardware/qcom/utils.mk
 
 include $(display-hal)/Android.mk
 include $(call all-makefiles-under,$(audio-hal))
-include $(call all-makefiles-under,$(gps-hal))
 include $(call all-makefiles-under,$(media-hal))
 endif
