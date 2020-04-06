84bcb76bf61def0e501c68db0e7e10ada94f11ea
sjllls <2012ji123@gmail.com>
2020-04-06 15:00:32 +0800

    rootdir: move apps permissions to vendor

--- common-perm.mk
+++ common-perm.mk
@@ -44,14 +44,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml
 
-# Common System Permissions
-PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/rootdir/system/etc/permissions/privapp-permissions-extension.xml:system/etc/permissions/privapp-permissions-extension.xml
-
 # Transmit power
 PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/rootdir/system/etc/permissions/privapp-permissions-transmitpower.xml:system/etc/permissions/privapp-permissions-transmitpower.xml
+    $(COMMON_PATH)/rootdir/vendor/etc/permissions/privapp-permissions-transmitpower.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/privapp-permissions-transmitpower.xml
 
 # Extended Settings
 PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/rootdir/system/etc/permissions/privapp-permissions-extendedsettings.xml:system/etc/permissions/privapp-permissions-extendedsettings.xml
+    $(COMMON_PATH)/rootdir/vendor/etc/permissions/privapp-permissions-extendedsettings.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/privapp-permissions-extendedsettings.xml
